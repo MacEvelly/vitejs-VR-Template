4c771acbf3b929d72545d9df654d7e46feae9f49
MacEvelly <MacEvelly@gmail.com>
2023-02-24 16:28:51 -0600

    Added Instances

---
+++ src/assets/index.jsx
@@ -0,0 +1,8 @@
+/****************************************************************** GLB Files */
+export { default as VRTemplate } from './glb/VRTemplate.glb';
+
+/****************************************************************** Font Files */
+export { default as Font } from './fonts/font.json';
+
+/****************************************************************** Audio Files */
+export { default as Audio } from './audio/Atmosphere_Stereo.ogg';
